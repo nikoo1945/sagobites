1e3a6d0ece889209707fa21a8fe404c58502282d
Nico <nikoo1945@github.com>
2026-03-05 02:11:24 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sagobites/',
+  build: {
+    assetsDir: 'assets'
+  },
   server: {
     port: 3000,
     host: true
